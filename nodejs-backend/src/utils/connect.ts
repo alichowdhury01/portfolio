9ba2bc9b2727f702ac168db8f1f040f82087f794
alichowdhury01 <39740365+alichowdhury01@users.noreply.github.com>
2024-05-22 10:43:37 -0400

    Update connect.ts

--- nodejs-backend/src/utils/connect.ts
+++ nodejs-backend/src/utils/connect.ts
@@ -10,8 +10,9 @@ import logger from './logger';
 async function connect() {
   const dbUri = config.get<string>('dbUri');
   const collectionName = config.get<string>('collectionName');
+  console.log(collectionName)
   try {
-    await mongoose.connect('mongodb+srv://alichowdhury:Mouri123!@bd1.gm4lics.mongodb.net/?retryWrites=true&w=majority&appName=bd1')
+    await mongoose.connect(dbUri, { dbName: collectionName })
 
     logger.info('DB connected');
   } catch (error) {
