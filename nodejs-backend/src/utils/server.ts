e381ed55625efc62ff49b297e7e27625bfbf5233
alichowdhury01 <39740365+alichowdhury01@users.noreply.github.com>
2024-05-23 16:08:41 -0400

    Update server.ts

--- nodejs-backend/src/utils/server.ts
+++ nodejs-backend/src/utils/server.ts
@@ -8,7 +8,7 @@ function createServer() {
   const app = express();
   app.use(
     cors({
-      origin: "http://localhost:3000",
+      origin: "http://www.alichowdhury.ca",
       credentials: true,
     })
   );
