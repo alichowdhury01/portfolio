938d138c9e11964e5c009f1ac13aa893845ea943
alichowdhury01 <39740365+alichowdhury01@users.noreply.github.com>
2024-05-23 18:54:15 -0400

    Update server.ts

--- nodejs-backend/src/utils/server.ts
+++ nodejs-backend/src/utils/server.ts
@@ -5,14 +5,18 @@ import cors from 'cors';
 import config from 'config';
 
 function createServer() {
-  const app = express();
-  app.use(
-    cors({
-      origin: "rest-api.alichowdhury.net",
-      credentials: true,
-    })
-  );
+  const allowedOrigins = ['https://www.alichowdhury.ca'];
 
+  const corsOptions: cors.CorsOptions = {
+    origin: allowedOrigins,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type,Authorization',
+    credentials: true,
+  };
+
+  const app = express();
+  
+  app.use(cors(corsOptions));
   app.use(express.json());
   app.use(deserializeUser);
   routes(app);
