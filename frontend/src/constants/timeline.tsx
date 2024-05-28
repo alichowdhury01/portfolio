904a56b790cf5bc2ddad9c7ba7f00ebe45f12631
alichowdhury01 <39740365+alichowdhury01@users.noreply.github.com>
2024-05-28 16:55:39 -0400

    portfolio

---
+++ frontend/src/constants/timeline.tsx
@@ -0,0 +1,62 @@
+export const timeline = [
+  {
+    company: "FLO",
+    title: "Customer Experience Specialist Agent",
+    date: "2023 - Present",
+    department: "FLO – CUSTOMER EXPERIENCE TEAM",
+    responsibilities: [
+      "Working closely with various stakeholders and teams to stay informed about new offers, services, and procedures.",
+      "Managing phone calls and emails from EV drivers, administrative contacts, or EV charging station owners.",
+      "Assessing and pre-assigning B2B opportunities to the sales team.",
+      "Conducting thorough diagnostics and resolving first-level support incidents within a specified response time, adhering to company quality standards."
+    ],
+  },
+  { 
+    company: "LevelUp Analytics",
+    title: "FRONTEND DEVELOPER Internship",
+    date: "2023 - 2023",
+    department: "DEV TEAM",
+    responsibilities: [
+      "Developed and implemented front-end features using Angular, transitioning from the previous platform to the new one.",
+      "Designed and created wireframes for a key feature, ensuring a user-friendly interface and optimal user experience.",
+      "Collaborated with the back-end team to ensure the seamless integration of front-end and back-end features.",
+      "Collaborated with cross-functional teams to gather requirements, incorporate feedback, and iterate on design and functionality."
+    ],
+  },
+  {
+    company: "CBC/RADIO-CANADA",
+    title: "Technical Support Specialist",
+    date: "2021 - 2021",
+    department: "MEDIA SUPPORT TEAM",
+    responsibilities: [
+      "Provide technical support to users, assisting them with troubleshooting and problem-solving on our media platform.",
+      "Document customer comments, feedback, and requests to gather data for statistical analysis.",
+      "Monitor individual customer accounts, including billing and credit card payments, through our management systems."
+
+    ],
+  },
+  {
+    company: "GSoft",
+    title: "Customer Support Agent",
+    date: "2020 - 2021",
+    department: "CUSTOMER SUCCESS - SHAREGATE TEAM",
+    responsibilities: [
+      "Actively listen to customers with empathy, demonstrating strong communication skills in both French and English, whether over the phone or in writing.",
+      "Employ analytical thinking to understand and address customer inquiries, resolving technical issues related to ShareGate products.",
+      "Collaborate with daily team meetings, actively participating to help kickstart the day and align on goals and tasks.",
+      "Demonstrate a proactive approach to problem-solving, actively seeking opportunities to improve customer experience and product support."
+    ],
+  },
+  {
+    company: "B2B2C",
+    title: "Provisioning Specialist",
+    date: "2018 - 2019",
+    department: "PROVISIONING TEAM",
+    responsibilities: [
+      "Efficiently place orders for installations and equipment changes with various partners such as Bell, Videotron, and Cogeco.",
+      "Skillfully process and manage requests for internet and VoIP service activations, ensuring prompt and accurate provisioning of services.",
+      "Enhanced the office suite to improve productivity.",
+      "Proficiently handle and manage DOCSIS, DSL, and VoIP modem equipment, including setup, configuration, and troubleshooting.",
+    ],
+  },
+];
