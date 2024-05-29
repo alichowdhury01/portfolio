2379f28e00e8d74d81c9be6779c50181c5f2456c
alichowdhury01 <39740365+alichowdhury01@users.noreply.github.com>
2024-05-29 10:27:32 -0400

    deploy

--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-
-
 echo "Pulling latest changes from repository"
 git pull origin main
 
@@ -23,7 +21,7 @@ if [ "$(docker ps -q -f name=caddy-service)" ]; then
 fi
 
 echo "Building and starting Docker containers"
-docker compose up --build --no-cache -d
+docker-compose up --build --no-cache -d
 
 echo "Verifying deployment"
-docker compose logs
+docker-compose logs
